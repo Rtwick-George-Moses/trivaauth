d7fc90d3b4fe4dc25787ebab71e9224e4d6b41b6
Rtwick George Moses <rtwick612@gmail.com>
2024-04-29 03:14:40 +0530

    push2 final

---
+++ src/app/components/header.jsx
@@ -0,0 +1,51 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="text-center sm:text-left">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Hi,Backend still being built!
+            </h1>
+
+            <p className="mt-1.5 text-sm text-gray-500">
+              Here is how it looks for now! 🎉
+            </p>
+          </div>
+
+          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+            <a
+              href="https://www.linkedin.com/in/rtwick-g-moses/"
+              className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 bg-white px-5 py-3 text-gray-500 transition hover:text-gray-700 focus:outline-none focus:ring"
+            >
+              <span className="text-sm font-medium"> View LinkedIn </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
+            </a>
+
+            <a
+              href="mailto: rtwick612@gmail.com"
+              className="block rounded-lg bg-orange-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-orange-700 "
+              type="button"
+            >
+              Email
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
